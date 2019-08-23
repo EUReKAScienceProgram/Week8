85589108e3c4291e8bbb2163fd967d7c3e2fcb3e
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-08-23 16:32:02 -0700

    Added files to Gamora - Reed

---
+++ Projects/Gamora/test.java
@@ -0,0 +1,20 @@
+public class test {
+public static void main(String[] args){
+System.out.println("memes");
+double oooLookAtThis = 10.593;
+String Eureka = "pie reed"; 
+int memes = 17;  
+System.out.println( memes);
+System.out.println(memes);
+System.out.println(oooLookAtThis);
+int result = dankmemes(9382, 104);
+System.out.println(result);
+}
+
+public static int dankmemes(int Numberone,int numberTwo){
+return NumberOne *numberTwo;
+
+}
+
+
+}
