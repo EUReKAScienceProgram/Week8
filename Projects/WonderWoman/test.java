67997901e63ea469f83d14b1d74256c7600d65f8
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-08-23 16:58:55 -0700

    Added files to wonder woman - Reed

---
+++ Projects/WonderWoman/test.java
@@ -0,0 +1,51 @@
+public class test {
+
+public static void main(String[] args){
+
+
+
+
+
+
+int endgame = 38;
+
+
+
+
+System.out.println (endgame);
+
+System.out.println(hiiiiii);
+
+
+System.out.println(EUReKA)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int result = endgame 100, 200
+System.out.println(result);
+
+}
+
+public static int endgame (int numberOne, int numberTwo){
+
+
+return numberOne * numberTwo;
+
+}
+
+
+
+}
